9149ab262d3dfec522ca13c5513ed9898f2a58fb
sandr <sandres0190@gmail.com>
2022-04-07 16:41:58 -0500

    Second Commit

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,10 +2,49 @@ package com.example.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import kotlin.random.Random
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(){
+
+    private lateinit var resultText: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView( R.layout.activity_main )
+
+        resultText = findViewById( R.id.tv_result )
+
+        val rollButton: Button = findViewById( R.id.btn_roll )
+
+        val countUpButton: Button = findViewById( R.id.btn_count_up)
+
+        rollButton.setOnClickListener{ rollDice() }
+
+        countUpButton.setOnClickListener {
+            countUp()
+        }
+
+    }
+
+    private fun countUp() {
+
+        resultText.apply {
+            when(this.text){
+                "Hello World" -> this.text = "1"
+                "6" -> this.text = "6"
+                else -> this.text = ( this.text.toString().toInt() + 1 ).toString()
+            }
+        }
+
+    }
+
+    private fun rollDice(){
+
+        resultText.text = (1..20).random().toString()
+        //Toast.makeText(this, "Button clicked", Toast.LENGTH_SHORT).show();
     }
 }
